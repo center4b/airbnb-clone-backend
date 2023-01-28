99a87adce6e9124e1aa3b558187fdebdd10e5e66
Jeong Sangwoo <center_4b@icloud.com>
2023-01-28 11:18:29 +0900

    5.6 Recap (230128)
    relationship study

--- houses/admin.py
+++ houses/admin.py
@@ -4,11 +4,6 @@ from .models import House
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
     
-    fields = (
-        "name", 
-        "address", 
-        ("price_per_night", "pets_allowed"),
-    )
     list_display = (
         "name",
         "price_per_night",
